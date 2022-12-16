4266029052da75cae6a5012656de78924ca4699a
user <user@LAPTOP-U0CO45L6>
2022-12-16 18:03:32 +0900

    asdas

--- test/src/main/java/lee/jun/ho/board/service/impl/BoardServiceImpl.java
+++ test/src/main/java/lee/jun/ho/board/service/impl/BoardServiceImpl.java
@@ -275,7 +275,8 @@ public class BoardServiceImpl implements BoardService{
 		
 		for(int i=0; i<fileArr.length; i++) {
 			log.info("fileArr ::: " + fileArr[i]);
-			//배열데이터를 맵에 넣어보기 mapData.put(boardNum, fileArr);
+			////배열데이터를 맵에 넣어보기 mapData.put(boardNum, fileArr);
+			
 		}
 		
 		
